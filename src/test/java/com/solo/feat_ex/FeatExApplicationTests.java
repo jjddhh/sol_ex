addf81350563bd5d95f476f27aae86fd8328da23
ehdgh <ehdgh25@naver.com>
2022-07-15 11:26:23 +0900

    first commit

---
+++ src/test/java/com/solo/feat_ex/FeatExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solo.feat_ex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeatExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
